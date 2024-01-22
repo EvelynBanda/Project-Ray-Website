25add726a7e89f01b6030e4dae638bb6367a5e42
EvelynBanda <evbanda@enfint.tech>
2024-01-22 15:40:09 +0200

    Third commit

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,12 +1,33 @@
-function getProducts() {
+const Product = require('../models/product.model');
 
+function getProducts(req, res) {
+    res.render('admin/products/all-products');
 }
 
-function getNewProduct() {
-
+function getNewProduct(req, res) {
+    res.render('admin/products/new-product')
 }
 
-function createNewProduct() {
+async function createNewProduct(req, res, next) {
+    // console.log(req.body);
+    // console.log(req.file);
+ const product = new Product ({
+    ...req.body,
+    image: req.file.filename
+ });
+  try{ 
+    await product.save();
+}   catch (error) {
+    next(error);
+    return;
+}
 
+    res.redirect('/admin/products');
+}
+
+module.exports = {
+    getProducts: getProducts,
+    getNewProduct: getNewProduct,
+    createNewProduct: createNewProduct
 }
 
