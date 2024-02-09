1e80f9674c490172d64f90d87f3e53c551b66ca7
EvelynBanda <evbanda@enfint.tech>
2024-02-09 02:51:43 +0200

    Sixth commit: Fixes and outputting products for customers as well as the product details

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -40,13 +40,44 @@ async function getUpdateProduct(req, res, next) {
     }
 };
 
-function updateProduct() {};
+async function updateProduct(req, res, next) {
+    const product = new Product({
+       ...req.body,
+       _id: req.params.id 
+    });
+
+    if (req.file) {
+        product.replaceImage(req.file.filename);
+    }
+
+    try{
+      await product.save();
+    } catch (error) {
+      next(error);
+      return;
+    }
+    res.redirect('/admin/products');
+}
+
+    async function deleteProduct(req, res, next) {
+        let product;
+     try {
+       product = await Product.findById(req.params.id);
+       await product.remove();
+     } catch (error) {
+        return next(error);
+     }
+
+     res.json({message: 'Deleted product!'});
+    }
+
 
 module.exports = {
     getProducts: getProducts,
     getNewProduct: getNewProduct,
     createNewProduct: createNewProduct,
     getUpdateProduct: getUpdateProduct,
-    updateProduct: updateProduct
+    updateProduct: updateProduct,
+    deleteProduct: deleteProduct
 }
 
